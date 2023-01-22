525b55ca9a10f13c3a9983b3ce73c7400850e83e
ako <jaleksandre@yahoo.com>
2023-01-22 11:40:53 +0400

    some javascript

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const variable = ცვლადი;
+const variable = "ცვლადი";
 
-axali  
+let variableTwo;
 
+
+console.log(variableTwo);
+
+function calculateNumbers(num1, num2) {
+    const calculatedValue = num1 + num2;
+console.log(calculatedValue);
+}
+calculateNumbers(11, 31)
+
+
+// calculateNumbers("hello world", 15)
+
+const addNumbers = function(num1, num2) {
+    console.log(num1 + num2);
+}
+
+addNumbers(10, 15)
+
+const multiplyNumbers = (num1, num2) => {
+    const calculatedValue = num1 + num2;
+    console.log(calculatedValue)
+};
+
+multiplyNumbers(10, 12)
