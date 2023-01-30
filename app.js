e6f6303924fe0845a860fc582ec86ee5173bb06c
ako <jaleksandre@yahoo.com>
2023-01-30 14:45:34 +0400

    davaleba

--- app.js
+++ app.js
@@ -1,15 +1,78 @@
-/* < > == === >= <= !== */
+// დავალება 1
 
-const num1 = 5
-const num2 = 5
-const result = num1 + num2
+// function simpleCalculator () {
+//     let firstNumber = Number(prompt("enter first number: "))
+//     let secondNumber = Number(prompt("enter second number: "))
+//     let mathOperator = prompt(" / * - + ")
+
+//     if(mathOperator === "/"){
+//         return firstNumber / secondNumber
+//     }
+//    else if(mathOperator === "*"){
+//    return firstNumber*secondNumber
+//     }
+//   else  if(mathOperator === "+"){
+//         return firstNumber + secondNumber
+//     }
+//    else if(mathOperator === "-"){
+//         return firstNumber - secondNumber
+//     }
+// }
+
+// let result = simpleCalculator()
+// console.log(`this is result ${result}`);
+
+// console.log(simpleCalculator());
 
 
+//  დავალება 2
 
-if (num1 !== num2) {
-console.log("not equal");
-} else if(num1 >= num2) {
-    console.log("result")
-}else if (result <= 10) {
-    console.log("some else statment");
-}
+// function someNumbers(ako) {
+//     let sum = 0
+
+// for (let index = 0; index < ako.length; index++) {
+//    sum = sum + ako[index]
+    
+// }
+// return sum 
+// } 
+// let numbers=[10, 15, 20, 25]
+
+// console.log(someNumbers(numbers));
+
+
+// დავალება 3
+
+// function vowelCount(str1){
+//   let vowelList = "aeiouAEIOU";
+  
+//   let count = 0;
+  
+//   for(let x = 0; x < str1.length ; x++)
+//   {
+//     if (vowelList.indexOf(str1[x]) !== -1)
+//     {
+//       count += 1;
+//     }
+  
+//   }
+//   return count;
+// }
+
+// console.log(vowelCount("The Heroes are humans in movies"));
+
+// დავალება 4
+
+// function reverseArray(arr) {
+//     let reversed = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//       reversed.push(arr[i]);
+//     }
+//     return reversed;
+//   }
+    
+//   let strings = ["apple", "banana", "cherry"];
+    
+//   let result = reverseArray(strings);
+  
+//   console.log(result);
